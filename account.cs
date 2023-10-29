77f653481cb11e252f68c2581254a7907b52a91e
Shahhdd <149313947+Shahhdd@users.noreply.github.com>
2023-10-29 17:47:50 +0300

    Add files via upload

---
+++ account.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BMS
+{
+    abstract class Account
+    {
+        public readonly string name;
+        public readonly string ID;
+        public readonly DOB DOB;
+        public readonly string nominee;
+        public double balance;
+        static string type =" Savings";
+        public double ammount;
+        public abstract bool deposit(double amount);
+
+        public abstract bool withdraw(double amount);
+
+        public double getBalance()
+        {
+            return balance;
+        }
+       public string getAccType(string type)
+        {
+
+            return type;
+        }
+        public void printAccount()
+        {
+
+            Console.WriteLine("Name : " + name);
+            Console.WriteLine("Date of Birth :" + DOB);
+            Console.WriteLine("Nominee : " + nominee);
+            Console.WriteLine("Balance :" + balance);
+        }
+        public Account()
+        {
+
+        }
+        public Account(string name, DOB DOB, string nominee, double balance )
+        {
+            this.name = name;
+            this.DOB = DOB;
+            this.nominee = nominee;
+            this.balance = balance;
+          
+        }
+    }
+}
---
+++ bank.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BMS
+{
+    class Bank
+    {
+        string id;
+        public int idnum = 0;
+
+        public string[] myId = new string[100];
+        public string[] myName = new string[100];
+        public string[] myAccType = new string[100];
+        public string[] myDob = new string[100];
+        public string[] myNominee = new string[100];
+        public double[] myBalance = new double[100];
+
+        IDGenerator Id = new IDGenerator();
+        DOB dob = new DOB();
+        Savings sv = new Savings();
+
+        public bool val = true;
+        public bool debval = true;
+
+        private void GetAcc(string ID)
+        {
+            ID = this.id;
+            myId[idnum] = ID;
+            idnum++;
+
+        }
+        public void showAll()
+        {
+            Console.WriteLine("All accounts are:\n");
+            for (int i = 0; i < idnum; i++)
+            {
+                Console.WriteLine(myId[i]);
+
+            }
+        }
+
+        public void showInfo()
+        {
+            int indexNum;
+            string inId = Convert.ToString(Console.ReadLine());
+            if (myId.Contains(inId))
+            {
+                indexNum = Array.IndexOf(myId, inId);
+                Console.WriteLine("Your details: ");
+                Console.WriteLine("Name: " + myName[indexNum]);
+                Console.WriteLine("Id: " + myId[indexNum]);
+                Console.WriteLine("Acc Type: " + myAccType[indexNum]);
+                Console.WriteLine("DOB: " + myDob[indexNum]);
+                Console.WriteLine("Nominee: " + myNominee[indexNum]);
+                Console.WriteLine("Balance: " + myBalance[indexNum]);
+            }
+            else
+            {
+                Console.WriteLine("Your id is wrong!");
+            }
+
+
+        }
+
+        public void create_account()
+        {
+
+          
+            string name;
+            int d,m,y;
+            string nominee;
+            double balance;
+            string accType = "Savings";
+            myAccType[idnum] = accType;
+            Console.Write("Name:");
+            name = Convert.ToString(Console.ReadLine());
+            myName[idnum] = name;
+            while (val == true)
+            {
+                Console.WriteLine("Enter date:");
+                d = Convert.ToInt32(Console.ReadLine());
+                m = Convert.ToInt32(Console.ReadLine());
+                y = Convert.ToInt32(Console.ReadLine());
+                dob.set(d, m, y);
+                if (dob.printDate() == false)
+                {
+                    myDob[idnum] = Convert.ToString(d + "-" + m + "-" + y);
+                    val = false;
+                }
+                else val = true;
+            }
+            val = true;
+            Console.WriteLine("Enter Nominee name: ");
+            nominee = Convert.ToString(Console.ReadLine());
+            myNominee[idnum] = nominee;
+            Console.WriteLine("Enter account balance: ");
+            balance = Convert.ToDouble(Console.ReadLine());
+            myBalance[idnum] = balance;
+            Console.WriteLine("Created Savings account successfully...! ");
+            id = Id.generate();
+            id = id + "Sav";
+            Console.WriteLine("Your Account Id : " + id);
+            GetAcc(id);
+
+            }
+
+        public void deposit()
+        {
+            int indexNum;
+            string inId = Convert.ToString(Console.ReadLine());
+            if (myId.Contains(inId))
+            {
+                indexNum = Array.IndexOf(myId, inId);
+                Console.WriteLine("Your Balance is: " + myBalance[indexNum]);
+                Console.WriteLine("How much you want to deposit: ");
+                double depval = Convert.ToDouble(Console.ReadLine());
+                if (myAccType[indexNum] == "Savings")
+                {
+                    sv.balance = myBalance[indexNum];
+                    sv.deposit(depval);
+                    myBalance[indexNum] = sv.balance;
+                }
+
+            }
+
+            else
+            {
+                Console.WriteLine("Your id is wrong!");
+            }
+
+        }
+        public void withdraw()
+        {
+            int indexNum;
+            string inId = Convert.ToString(Console.ReadLine());
+
+            if (myId.Contains(inId))
+            {
+                indexNum = Array.IndexOf(myId, inId);
+                Console.WriteLine("Your Balance is: " + myBalance[indexNum]);
+                Console.WriteLine("How much you want to withdraw: ");
+                double depval = Convert.ToDouble(Console.ReadLine());
+                if (myAccType[indexNum] == "Savings")
+                {
+                    sv.balance = myBalance[indexNum];
+                    sv.withdraw(depval);
+                    myBalance[indexNum] = sv.balance;
+                }
+
+
+                else
+                {
+                    Console.WriteLine("Your id is wrong!");
+                }
+
+            }
+        }
+    }
+}
